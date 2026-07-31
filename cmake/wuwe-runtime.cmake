741aed0e0e590d74f7de970325de4110d95a8c10
lkimuk <lkimuk@cppmore.com>
2026-07-31 18:19:20 +0800

    build(cmake): support source and installed consumers

--- cmake/wuwe-runtime.cmake
+++ cmake/wuwe-runtime.cmake
@@ -3,7 +3,7 @@ function(wuwe_copy_runtime_dlls target)
         COMMAND ${CMAKE_COMMAND}
             -Druntime_dlls="$<TARGET_RUNTIME_DLLS:${target}>"
             -Dtarget_dir="$<TARGET_FILE_DIR:${target}>"
-            -P "${CMAKE_SOURCE_DIR}/cmake/copy-runtime-dlls.cmake"
+            -P "${PROJECT_SOURCE_DIR}/cmake/copy-runtime-dlls.cmake"
         COMMAND_EXPAND_LISTS
     )
 endfunction()
